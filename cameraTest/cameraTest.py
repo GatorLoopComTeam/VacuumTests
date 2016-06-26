732d3ccf02060bd241aa750d1c2edbe44c885f5b
gdscheele <gdscheele@gmail.com>
2016-06-26 21:20:22 +0000

    Initial commit

---
+++ cameraTest/cameraTest.py
@@ -0,0 +1,14 @@
+import picamera
+import time
+
+camera = picamera.PiCamera()
+camera.vflip = True
+camera.start_preview()
+
+i = 0
+while True:
+	camera.capture('images/image' + str(i) + '.jpg')
+	i = i + 1
+	time.sleep(1)
+
+camera.stop_preview()
